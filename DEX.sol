8babc113d4a63b2288ce18097cb4b2af52ac2766
Hoonatdefy <hoon@defylabs.io>
2019-11-04 19:56:41 +0900

    Fix to build with solc

--- DEX.sol
+++ DEX.sol
@@ -1,8 +1,124 @@
 pragma solidity ^0.4.24;
 
-import "./ERC20Interface.sol";
-import "./SafeMath.sol";
-import "./DefyAccount.sol";
+interface ERC20 {
+    function totalSupply() external view returns (uint supply);
+    function balanceOf(address _owner) external view returns (uint balance);
+    function transfer(address _to, uint _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
+    function approve(address _spender, uint _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint remaining);
+    function decimals() external view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
+    }
+}
+
+interface AccountLevels {
+    function accountLevel(address user) public view returns(uint256);
+}
+
+contract AccountLevelStorage is AccountLevels {
+    mapping (address => uint256) public accountLevels;
+
+    function accountLevel(address user) public view returns(uint256) {
+        return accountLevels[user];
+    }
+
+    function setAccountLevel(address user, uint256 level) public {
+        accountLevels[user] = level;
+    }
+}
 
 contract DefyDelta {
     using SafeMath for uint;
@@ -94,11 +210,11 @@ contract DefyDelta {
         uint feeRebateXfer = 0;
 
 
-        if (accountLevelsAddr != 0x0) {
-            uint accountLevel = AccountLevels(accountLevelsAddr).accountLevel(user);
-            if (accountLevel==1) feeRebateXfer = amount.mul(feeRebate) / (1 ether);
-            if (accountLevel==2) feeRebateXfer = feeTakeXfer;
-        }
+        // if (accountLevelsAddr != 0x0) {
+        //     uint accountLevel = AccountLevels(accountLevelsAddr).accountLevel(user);
+        //     if (accountLevel==1) feeRebateXfer = amount.mul(feeRebate) / (1 ether);
+        //     if (accountLevel==2) feeRebateXfer = feeTakeXfer;
+        // }
 
         tokens[tokenGet][msg.sender] = tokens[tokenGet][msg.sender].sub(amount.add(feeTakeXfer));
 
